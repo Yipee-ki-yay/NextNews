74841bca36298a608abf70d5dfe345a996bdbbf1
Oleksandr Morozov <mopoz07@gmail.com>
2024-05-27 18:37:14 +0300

    add granular data fetching with suspense

--- app/(content)/news/[slug]/page.js
+++ app/(content)/news/[slug]/page.js
@@ -1,10 +1,11 @@
-import { DUMMY_NEWS } from "@/dummy-news"
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export default function NewsDetailsPage({params}) {
+import { getNewsItem } from "@/lib/news";
+
+export default async function NewsDetailsPage({params}) {
   const newsSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug);
+  const newsItem = await getNewsItem(newsSlug)
 
   if(!newsItem) {
     notFound();
